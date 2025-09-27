c439ff32071c410da29bed1166e4f89f3c4c955a
huynguyenj <nguyenhuyjobs@gmail.com>
2025-09-27 11:01:55 +0700

    error-handler: finish http-exception-error and all-exeption-error

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './exception-filter/http.exception.filter';
+import { AllExceptionFilter } from './exception-filter/all.exception.filter';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const httpAdapter = app.get(HttpAdapterHost);
+  const config = app.get(ConfigService);
+  app.useGlobalFilters(
+    new HttpExceptionFilter(config),
+    new AllExceptionFilter(httpAdapter),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
