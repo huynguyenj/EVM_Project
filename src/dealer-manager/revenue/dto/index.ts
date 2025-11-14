782f5da071c16d6d189f704c3cb52e64f855edd2
huynguyenj <nguyenhuyjobs@gmail.com>
2025-11-14 21:39:38 +0700

    fix: database table

--- src/dealer-manager/revenue/dto/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './request/create-revenue-dto';
-export * from './request/update-revenue-dto';
-export * from './request/revenue-query';
-export * from './response/revenue-response-dto';
--- src/dealer-manager/revenue/dto/request/revenue-query.ts
+++
@@ -1,7 +0,0 @@
-import { PaginationRequestQuery } from 'src/common/types';
-
-export class RevenueQueries implements PaginationRequestQuery {
-  limit: number;
-  page: number;
-  year?: number;
-}
